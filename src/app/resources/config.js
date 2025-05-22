0e8b7d81e8ed2b4154faf2d32100d8eeb7f54ad8
VannTakashi <andreanrevan276@gmail.com>
2025-05-22 21:18:24 +0700

    Initial Rebase
    Signed-off-by: VannTakashi <andreanrevan276@gmail.com>

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -11,9 +11,6 @@ const routes = {
 
 // Enable password protection on selected routes
 // Set password in the .env file, refer to .env.example
-const protectedRoutes = {
-  "/work/automate-design-handovers-with-a-figma-to-code-pipeline": true,
-};
 
 import { Geist } from "next/font/google";
 import { Geist_Mono } from "next/font/google";
